f0d4307ee210f702a6af0630828d34ba39811d0f
nwiley <kononovap7@gmail.com>
2023-09-23 21:41:40 +0700

    create

---
+++ Game/Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Полина on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
